6258856b59580788e39e817d53791ba3fd833018
Jessica Manak <jessica8angels@gmail.com>
2021-10-15 06:06:16 -0700

    finished links and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import Form from "./Form"
+
+
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <p>You can order a pizza online! It's like we're in the movie, The Net!</p>
+      <Link to='/'>Home</Link>
+      <Link to='/pizza'> Build Your Own Pizza </Link>
+      <Route exact path='/'></Route>
+      <Route path='/pizza'>
+        <Form />
+      </Route>
+    </BrowserRouter>
   );
 };
 export default App;
---
+++ src/Form.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Pizza(props) {
+    return (
+        <div className='form'>
+            <h3>What do you want on your Tombstone?</h3>
+
+        </div>
+    )
+}
